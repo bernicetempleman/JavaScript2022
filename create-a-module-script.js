b24cecd6931e71eca1bc14aaee4517022b54ede0
Bernice Templeman <bernice.templeman@gmail.com>
2022-09-11 17:09:40 -0400

    Add files via upload

---
+++ create-a-module-script.js
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <!-- Only change code below this line -->
+
+    <script type="module" src="index.js">
+    </script>
+
+    <!-- Only change code above this line -->
+  </body>
+</html>
